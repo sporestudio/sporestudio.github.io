f8ef9a093526a6906a135c002eed9c9ba55d8ba2
sporestudio <jrodcas@proton.me>
2025-01-26 00:10:22 +0100

    refactor: restore old version from backup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sporestudio.github.io',
-  base: '/',
-  integrations: [
-    react(),
-    tailwind(),
-    icon(),
-  ]
+  site: "https://sporestudio.github.io",
+  integrations: [react(), tailwind(), icon()],
 });
